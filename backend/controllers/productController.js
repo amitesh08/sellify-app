0dfd9b84df61e5af64633e14652d4d3d5bc482a7
amitesh08 <aamitesh125@gmail.com>
2024-11-13 18:12:07 +0530

    added routes for  deleting and updating the product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -68,3 +68,87 @@ export async function getProduct(req, res){
         client.release();
     }
 } 
+
+export async function deleteProduct(req, res){
+    const { id } = req.params;
+    const client = await connectDB();
+    try {
+        const deleteProductQuery = `
+            DELETE FROM products 
+            WHERE id = $1
+            RETURNING *;
+        `
+        const deleteValue = [ id ];
+
+        const result = await client.query(deleteProductQuery,deleteValue);
+
+        if(result.rowCount === 0 ){    //checking if the used id is correct.. | ->rowCount will give you affected row with the query.
+            return res.status(404).json({
+                success: false,
+                message: `Can not find the product with id ${id}` 
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: `product id = ${id} has been deleted successfully`
+        })
+    } catch (error) {
+        console.error("error deleting product", error);
+        res.status(500).json({
+            success: false,
+            message: 'failed  to delete products'
+        })
+    }
+    finally{
+        client.release();
+    }
+}
+
+export async function updateProduct(req, res){
+    const { id } = req.params;
+    const {name, price, image} = req.body;
+
+    if(!id || (!name && !price && !image)){
+        return res.status(400).json({
+            success: false,
+            message: "Please provide the product ID and at least one field to update."
+        })
+    }
+
+    const client = await connectDB();
+    try {
+        const updateProductQuery = `
+            UPDATE products 
+            SET
+                name = COALESCE($1, name),
+                price = COALESCE($2, price),
+                image = COALESCE($3, image),
+                updated_at = CURRENT_TIMESTAMP 
+            WHERE id = $4
+            RETURNING * ;
+        `
+        const updateValues = [name, price , image, id];
+        const result = await client.query(updateProductQuery,updateValues);
+
+        if(result.rowCount === 0 ){    //checking if the product exist.. | ->rowCount will give you affected row with the query.
+            return res.status(404).json({
+                success: false,
+                message: `Can not find the product with id ${id}` 
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "product updated successfully", 
+            product: result.rows[0]
+        })
+    } catch (error) {
+        console.error("error while updating product", error);
+        res.status(500).json({
+            success: false,
+            message: "failed to update product."
+        })
+    }
+    finally{
+        client.release()
+    }
+}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { addProduct, getProduct } from "../controllers/productController.js";
+import { addProduct, deleteProduct, getProduct, updateProduct } from "../controllers/productController.js";
 
 
 const router = express.Router();
 
 router.post("/",addProduct);
 router.get("/", getProduct);
+router.delete("/:id", deleteProduct)
+router.put("/:id", updateProduct)
 
 export default router;
